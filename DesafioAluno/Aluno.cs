bd4b01da19a414e396833f21543296ca9cb141d2
Leila Contel <leeilacontell@gmail.com>
2022-08-15 11:26:46 -0300

    Desafio extra 2

---
+++ DesafioAluno/Aluno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioAluno
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public int Idade { get; set; }
+        public double NotaMatematica { get; set; }
+        public double NotaPortugues { get; set; }
+        public double NotaGeografia { get; set; }
+
+        public Aluno(string nome, string endereco, int idade, double notaMatematica, double notaPortugues, double notaGeografia)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Idade = idade;
+            NotaMatematica = notaMatematica;
+            NotaPortugues = notaPortugues;
+            NotaGeografia = notaGeografia;
+        }
+
+        public string ImprimirNotas()
+        {
+            var notas =
+                $"Nome: {Nome} \nEndereco {Endereco} \nIdade: {Idade} \n" +
+                 $"Nota de matemática: {NotaMatematica.ToString("F1", CultureInfo.InvariantCulture)} \n" +
+                 $"Nota de português: {NotaPortugues.ToString("F1", CultureInfo.InvariantCulture)} \n" +
+                 $"Nota de geografia: {NotaGeografia.ToString("F1", CultureInfo.InvariantCulture)}";
+
+            return notas;
+        }
+
+        public string ImprimirNotas(char materia)
+        {
+            var entidade = "";
+
+            if (materia == 'G' || materia == 'g')
+            {
+                entidade = $"Nome: {Nome} \nEndereço: {Endereco} \nIdade: {Idade} \nNota de geografia: {NotaGeografia.ToString("F1",CultureInfo.InvariantCulture)}";
+            }
+            else if (materia == 'M' || materia == 'm')
+            {
+                entidade = $"Nome: {Nome} \nEndereço: {Endereco} \nIdade: {Idade} \nNota de matematica: {NotaMatematica.ToString("F1", CultureInfo.InvariantCulture)}";
+            }
+            else if (materia == 'P' || materia == 'p')
+            {
+                entidade = $"Nome: {Nome} \nEndereço: {Endereco} \nIdade: {Idade} \nNota de portugues {NotaPortugues.ToString("F1", CultureInfo.InvariantCulture)}";
+            }
+            return entidade;
+        }
+    }
+}
---
+++ DesafioAluno/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("NOME: ");
+            var Nome = Console.ReadLine();
+            Console.Write("ENDEREÇO: ");
+            var Endereco = Console.ReadLine();
+            Console.Write("IDADE: ");
+            var Idade = int.Parse(Console.ReadLine());
+            Console.Write("NOTA DE MATEMÁTICA: ");
+            var NotaMatematica = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("NOTA DE PORTUGUÊS: ");
+            var NotaPortugues = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("NOTA DE GEOGRAFIA: ");
+            var NotaGeografia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("");
+            Console.Write("ESCOLHA UMA OPÇÃO (M-MATEMATICA / P-PORTUGUES/ G-GEOGRAFIA): ");
+            var escolha = char.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Aluno aluno = new Aluno(Nome, Endereco, Idade, NotaMatematica, NotaPortugues, NotaGeografia);
+
+            Console.WriteLine(aluno.ImprimirNotas());
+            Console.WriteLine("");
+            Console.WriteLine(aluno.ImprimirNotas(escolha)); 
+        }
+    }
+}
+
+
+
+
