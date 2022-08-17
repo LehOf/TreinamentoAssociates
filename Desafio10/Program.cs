88b6b6024c5f11dfee8c703a52f8c6de689c677b
Leila Contel <leeilacontell@gmail.com>
2022-08-17 16:53:47 -0300

    Desafio Reino Animal

---
+++ Desafio10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Desafio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var especieFinal = "";
+
+            Console.Write("Digite a primeira especie: ");
+            string _primeiraEspecie = Console.ReadLine().ToLower();
+            Console.Write("Digite a segunda especie: ");
+            string _segundaEspecie = Console.ReadLine().ToLower();
+            Console.Write("Digite a terceira especie: ");
+            string _terceiraEspecie = Console.ReadLine().ToLower();
+            Console.WriteLine("");
+
+            ReinoAnimal especie = new ReinoAnimal(_primeiraEspecie, _segundaEspecie, _terceiraEspecie);
+
+            if (_primeiraEspecie == "vertebrado")
+            {
+                especieFinal = especie.EspecieDosVertebrados();
+            }
+            else if (_primeiraEspecie == "invertebrado")
+            {
+                especieFinal = especie.EspeciesDosInvertebrados();
+            }
+
+            Console.WriteLine($"{especieFinal}");
+        }
+    }
+}
---
+++ Desafio10/ReinoAnimal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Desafio10
+{
+    class ReinoAnimal
+    {
+        private string _PrimeiraClassificacao { get; set; }
+        private string _SegundaClassificacao { get; set; }
+        private string _TerceiraClassificacao { get; set; }
+        private string _ClassificacaoResultante { get; set; }
+
+        public ReinoAnimal(string _primeiraClassificacao, string _segundaClassificacao, string _terceiraClassificacao)
+        {
+            _PrimeiraClassificacao = _primeiraClassificacao;
+            _SegundaClassificacao = _segundaClassificacao;
+            _TerceiraClassificacao = _terceiraClassificacao;
+        }
+
+        public string EspecieDosVertebrados()
+        {
+            if (_PrimeiraClassificacao == "vertebrado" && _SegundaClassificacao == "ave" && _TerceiraClassificacao == "carnivoro")
+            {
+                _ClassificacaoResultante = "aguia";
+            }
+            else if (_PrimeiraClassificacao == "vertebrado" && _SegundaClassificacao == "ave" && _TerceiraClassificacao == "onivoro")
+            {
+                _ClassificacaoResultante = "pomba";
+            }
+            else if (_PrimeiraClassificacao == "vertebrado" && _SegundaClassificacao == "mamifero" && _TerceiraClassificacao == "onivoro")
+            {
+                _ClassificacaoResultante = "homem";
+            }
+            else if (_PrimeiraClassificacao == "vertebrado" && _SegundaClassificacao == "mamifero" && _TerceiraClassificacao == "herbivoro")
+            {
+                _ClassificacaoResultante = "vaca";
+            }
+
+            return _ClassificacaoResultante;
+
+        }
+        public string EspeciesDosInvertebrados()
+        {
+            if (_PrimeiraClassificacao == "invertebrado" && _SegundaClassificacao == "inseto" && _TerceiraClassificacao == "hematofago")
+            {
+                _ClassificacaoResultante = "pulga";
+            }
+            else if (_PrimeiraClassificacao == "invertebrado" && _SegundaClassificacao == "inseto" && _TerceiraClassificacao == "herbivaro")
+            {
+                _ClassificacaoResultante = "lagarta";
+            }
+            else if (_PrimeiraClassificacao == "invertebrado" && _SegundaClassificacao == "anelideo" && _TerceiraClassificacao == "hematofago")
+            {
+                _ClassificacaoResultante = "sanguessuga";
+            }
+            else if (_PrimeiraClassificacao == "invertebrado" && _SegundaClassificacao == "anelideo" && _TerceiraClassificacao == "onivoro")
+            {
+                _ClassificacaoResultante = "minhoca";
+            }
+
+            return _ClassificacaoResultante;
+        }
+    }
+}
