b6b4de0590209bbc44a7eb9fe8a7fb413bf4d9bd
Leila Contel <leeilacontell@gmail.com>
2022-08-22 14:29:33 -0300

    Desafio conversão de horas

---
+++ Desafio13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("DIGITE UM TEMPO EM SEGUNDOS: ");
+            var tempoEmSegundos = int.Parse(Console.ReadLine());
+
+            Tempo tempo = new Tempo(tempoEmSegundos);
+
+            tempo.ConversaoDoTempo();
+        }
+    }
+}
---
+++ Desafio13/Tempo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Desafio13
+{
+    class Tempo
+    {
+        private int _tempo { get; set; }
+
+        public Tempo(int tempo)
+        {
+            _tempo = tempo;
+        }
+
+        public void ConversaoDoTempo()
+        {
+            var horas = _tempo / 3600;
+            _tempo = _tempo % 3600;
+
+            var minutos = _tempo / 60;
+            _tempo = _tempo % 60;
+
+            var segundos = _tempo;
+
+            Console.WriteLine($"{horas} : {minutos} : {segundos} ");
+        }
+    }
+}
