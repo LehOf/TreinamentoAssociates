1462bff3f4c2f1e4c270a3f73ff9fe25cc313954
Leila Contel <leeilacontell@gmail.com>
2022-08-22 09:21:59 -0300

    Desafio Menor posicao

---
+++ Desafio12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("DIGITE A QUANTIDADE DE POSIÇOES DO VETOR: ");
+            int numeroDePosicoes = int.Parse(Console.ReadLine());
+
+            Vetor vetor = new Vetor(numeroDePosicoes);
+
+            Console.WriteLine(vetor);
+        }
+    }
+}
---
+++ Desafio12/Vetor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Desafio12
+{
+    class Vetor
+    {
+        private int _numeroDeposicoes { get; set; }
+        private int _menor { get; set; }
+        private int[] _posicoes { get; set; }
+
+        public Vetor(int numeroDePosicoes)
+        {
+            _numeroDeposicoes = numeroDePosicoes;
+        }
+
+        public int EncontrarMenorValorDoVetor(int numeroDePosicao)
+        {
+            Console.Write("DIGITE AS POSIÇÕES DO VETOR: ");
+            string[] posicoesDoVetor = Console.ReadLine().Split(' ');
+
+            _posicoes = new int[numeroDePosicao];
+            _menor = 1000;
+            var maior = 1;
+
+            for(int i=0; i<numeroDePosicao; i++)
+            {
+                _posicoes[i] = int.Parse(posicoesDoVetor[i]);
+
+                if (_menor > _posicoes[i])
+                {
+                    _menor = _posicoes[i];
+                }
+                else if(maior < _posicoes[i])
+                {
+                    maior = _posicoes[i];
+                }
+            }
+
+            return _menor;
+        }
+
+        public int EncontrarPosicaoMenorDoVetor(int numeroDePosicao)
+        {
+            var posicaoAlocada = 0;
+
+            for(int i=0; i<numeroDePosicao; i++)
+            {
+                if(_posicoes[i] == _menor)
+                {
+                    posicaoAlocada = i;
+                }
+            }
+
+            return posicaoAlocada;
+        }
+
+
+        public override string ToString()
+        {
+            return $"\n{EncontrarMenorValorDoVetor(_numeroDeposicoes)} \nPosição: {EncontrarPosicaoMenorValor(_numeroDeposicoes)}";
+        }
+    }
+}
+
+
