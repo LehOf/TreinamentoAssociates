3e0ed91112bfaaaa8613017b4ecb2447f956b180
Leila Contel <leeilacontell@gmail.com>
2022-08-23 15:43:59 -0300

    Desafio do quadrante

---
+++ Desafio14/Coordenadas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Desafio14
+{
+    class Coordenadas
+    {
+        public void ValorDoQuadrante()
+        {
+            var quadrante = "";
+
+            while (true)
+            {
+                Console.Write("Digite o valor da coordenada X e coordenada Y: ");
+                string[] coordenadas = Console.ReadLine().Split(' ');
+
+                var coordenadaX = int.Parse(coordenadas[0]);
+                var coordenadaY = int.Parse(coordenadas[1]);
+
+                if (coordenadaX != 0 && coordenadaY != 0)
+                {
+                    if (coordenadaX >= 0 && coordenadaY >= 0)
+                    {
+                        quadrante = "primeiro";
+                    }
+                    else if (coordenadaX <= 0 && coordenadaY >= 0)
+                    {
+                        quadrante = "segundo";
+                    }
+                    else if (coordenadaX <= 0 && coordenadaY <= 0)
+                    {
+                        quadrante = "terceiro";
+                    }
+                    else if (coordenadaX >= 0 && coordenadaY <= 0)
+                    {
+                        quadrante = "quarto";
+                    }
+                }
+                else
+                {
+                    break;
+
+                }
+
+                Console.WriteLine(quadrante);
+            }
+        }
+    }
+}
+
+
---
+++ Desafio14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Desafio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordenadas coordenada = new Coordenadas();
+
+            coordenada.ValorDoQuadrante();
+
+        }
+    }
+}
