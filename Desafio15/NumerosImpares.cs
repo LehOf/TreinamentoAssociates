b0b45da9f831032834b9c066557d800a48dd5709
Leila Contel <leeilacontell@gmail.com>
2022-08-25 12:35:31 -0300

    Desafio Numeros Impares

---
+++ Desafio15/NumerosImpares.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio15
+{
+    class NumerosImpares
+    {
+        private int _primeiroValor { get; set; }
+        private int _segundoValor { get; set; }
+
+        public NumerosImpares(int primeiroValor, int segundoValor)
+        {
+            _primeiroValor = primeiroValor;
+            _segundoValor = segundoValor;
+        }
+
+        public int SomaDosNumerosImpares()
+        {
+            var soma = 0;
+            var minValor = 0;
+            var maxValor = 0;
+
+            if(_primeiroValor < _segundoValor)
+            {
+                minValor = _primeiroValor;
+                maxValor = _segundoValor;
+            }
+            else
+            {
+                minValor = _segundoValor;
+                maxValor = _primeiroValor;
+            }
+
+            for(int numeros = minValor + 1; numeros < maxValor; numeros++)
+            {
+                if(numeros % 2 != 0)
+                {
+                    soma += numeros;
+                }
+
+            }
+
+            return soma;
+        }
+
+        public override string ToString()
+        {
+            return $"{SomaDosNumerosImpares()}";
+        }
+    }
+}
---
+++ Desafio15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor interio X: ");
+            var primeiroValor = int.Parse(Console.ReadLine());
+            Console.Write("Digite um valor inteiro Y: ");
+            var segundoValor = int.Parse(Console.ReadLine());
+
+            NumerosImpares numerosImpares = new NumerosImpares(primeiroValor, segundoValor);
+
+            numerosImpares.SomaDosNumerosImpares();
+
+            Console.WriteLine(numerosImpares);
+        }
+    }
+}
