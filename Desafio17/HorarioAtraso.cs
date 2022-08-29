9bf7d178249eb7e618e18a333f5467dbe4837359
Leila Contel <leeilacontell@gmail.com>
2022-08-29 10:24:30 -0300

    Desafio Domingo de manhà

---
+++ Desafio17/HorarioAtraso.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Desafio17
+{
+    class HorarioAtraso
+    {
+        private int _hora { get; set; }
+        private int _minuto { get; set; }
+
+        public HorarioAtraso(int hora, int minuto)
+        {
+            _hora = hora;
+            _minuto = minuto;
+        }
+
+        public string AtrasoBino()
+        {
+            var resultado = "";
+            var minutos = 0;
+            var horas = 0;
+
+            TimeSpan atraso, HorarioChegada, HorarioSaida, DuracaoMaxima, DuracaoMinima, HorarioLimiteMenor, HorarioLimiteMaior;
+
+            atraso = new TimeSpan(0, 0, 0);
+            HorarioChegada = new TimeSpan(8, 0, 0);
+            HorarioSaida = new TimeSpan(_hora, _minuto, 0);
+            DuracaoMaxima = new TimeSpan(1, 0, 0);
+            DuracaoMinima = new TimeSpan(0, 30, 0);
+            HorarioLimiteMenor = new TimeSpan(5, 0, 0);
+            HorarioLimiteMaior = new TimeSpan(9, 0, 0);
+
+            if (HorarioSaida >= HorarioLimiteMenor && HorarioSaida <= HorarioLimiteMaior)
+            {
+                if (HorarioSaida + (DuracaoMaxima) > HorarioChegada)
+                {
+                    atraso = HorarioSaida + (DuracaoMaxima);
+                    atraso = atraso - HorarioChegada;
+                    minutos = (int)atraso.TotalMinutes;
+                    resultado = $"Atraso maximo: {minutos} minutos";
+                }
+                else if (HorarioSaida + (DuracaoMinima) < HorarioChegada)
+                {
+                    resultado = "Atraso maximo = 0";
+                }
+            }
+            else
+            {
+                resultado = "Hoário fora do escopo limite";
+            }
+            return resultado;
+        }
+
+        public override string ToString()
+        {
+            return $"{AtrasoBino()}";
+        }
+    }
+}
---
+++ Desafio17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o horário que o Bino acordou?");
+            string[] valores = Console.ReadLine().Split(':');
+            int horas = int.Parse(valores[0]);
+            int minutos = int.Parse(valores[1]);
+
+            HorarioAtraso horario = new HorarioAtraso(horas, minutos);
+
+            Console.WriteLine(horario.AtrasoBino());
+
+        }
+
+        }
+}
