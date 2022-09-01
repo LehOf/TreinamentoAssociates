46224303f805f90d2521a9087325d87dd9dc6169
Leila Contel <leeilacontell@gmail.com>
2022-09-01 09:51:49 -0300

    Desafio arquivos

---
+++ Desafio20/Arquivo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Desafio20
+{
+    class Arquivo
+    {
+        private string _caminhoDoArquivo { get; set; }
+
+        public Arquivo(string caminho)
+        {
+            _caminhoDoArquivo = caminho;
+        }
+
+        public void LendoArquivo()
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(_caminhoDoArquivo))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                       var line = sr.ReadLine();
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void RetornandoTresNovosArquivos()
+        {
+
+
+        }
+    }
+}
---
+++ Desafio20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Desafio20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o caminho do arquivo:");
+            var SourcePath = Console.ReadLine();
+
+            var arquivo = new Arquivo(SourcePath);
+
+           arquivo.LendoArquivo();
+
+        }
+    }
+}
+
