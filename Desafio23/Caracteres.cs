3ca239bed924a860afc908d0b4ceae925cfa4e9e
Leila Contel <leeilacontell@gmail.com>
2022-09-05 12:13:07 -0300

    Desafio Contagem de Caracter

--- Desafio23/Caracteres.cs
+++ Desafio23/Caracteres.cs
@@ -6,57 +6,62 @@ namespace Desafio23
     {
         private string[] _frases { get; set; }
         private int _maior { get; set; }
-        private int _caracter { get; set; }
         private string _palavra { get; set; }
 
-        public Caracteres()
+        public Caracteres(string[] frases)
         {
-
+            _frases = frases;
         }
-
 
         public void ContarCaracterPalavra()
         {
+            var valor = "";
 
             do
             {
-                Console.WriteLine("");
-                Console.WriteLine("DIGITE UMA FRASE: ");
-                _frases = Console.ReadLine().Split(' ');
-
-                if (_frases[0] != "0")
+                foreach (var frase in _frases)
                 {
-                    foreach (var frase in _frases)
+                    if (frase.Length > 0)
                     {
-                        if (frase.Length > 0)
-                        {
-                            _maior = frase.Length;
-                            _palavra = frase;
-                        }
-                        Console.Write($"{frase.Length}-");
+                        _maior = frase.Length;
+                        _palavra = frase;
                     }
 
-                    for (int i = 0; i < _frases.Length; i++)
-                    {
-                        _caracter = _frases[i].Length;
+                    valor += $"{frase.Length}-";
+                }
+                Console.WriteLine(valor.Remove(valor.LastIndexOf("-"), 1));
 
-                        if (_caracter < _maior)
-                        {
-                            _maior = _maior;
-                            _palavra = _palavra;
-                        }
-                        else if (_caracter > _maior)
-                        {
-                            _maior = _caracter;
-                            _palavra = _frases[i];
-                        }
+                Console.WriteLine("\nDIGITE UMA FRASE: ");
+                _frases = Console.ReadLine().Split(' ');
+
+            } while (_frases[0] != "0");
+        }
+
+        public void MaiorPalavra()
+        {
+            for (int i = 0; i < _frases.Length; i++)
+            {
+                if (_frases[0] != "0")
+                {
+                   var caracter = _frases[i].Length;
+
+                    if (caracter < _maior)
+                    {
+                        _maior = _maior;
+                        _palavra = _palavra;
+                    }
+                    else if (caracter > _maior)
+                    { 
+                        _maior = caracter;
+                        _palavra = _frases[i];
                     }
                 }
                 else
                 {
                     Console.WriteLine($"The biggest word: {_palavra}");
                 }
-            } while (_frases[0] != "0");
+            }
         }
     }
 }
+
--- Desafio23/Program.cs
+++ Desafio23/Program.cs
@@ -6,10 +6,14 @@ namespace Desafio23
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("DIGITE UMA FRASE: ");
+            var frases = Console.ReadLine().Split(' ');
 
-            var caracter = new Caracteres();
+
+            var caracter = new Caracteres(frases);
 
             caracter.ContarCaracterPalavra();
+            caracter.MaiorPalavra();
         }
     }
 }
